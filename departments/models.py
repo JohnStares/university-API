fefd260645843ab09bb4540e8d61565d1fa69017
JohnStares <wisdom8achor24@gmail.com>
2025-10-10 10:21:34 +0100

    Initial models setup

--- departments/models.py
+++ departments/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
+from faculty.models import Faculties
+
 # Create your models here.
+class Departments(models.Model):
+    faculty = models.ForeignKey(Faculties, related_name="department_faculty", on_delete=models.CASCADE)
+    name = models.CharField(max_length=25, unique=True, null=False, blank=False)
+    date_created = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"Department of {self.name} in Faculty of {self.faculty.name}"

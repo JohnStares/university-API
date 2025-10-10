01edb595f409d9e615b10d7439d79dcace5eef94
JohnStares <wisdom8achor24@gmail.com>
2025-10-10 10:39:54 +0100

    Initial model setup for courses

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+from departments.models import Departments
+from faculty.models import Faculties
+
 # Create your models here.
+class Courses(models.Model):
+    title = models.CharField(max_length=30, null=False, blank=False, unique=True)
+    code = models.CharField(max_length=10, null=False, blank=False, unique=True)
+    units = models.IntegerField(max_length=3, null=False, blank=False)
+    date_created = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
+
+    departments = models.ForeignKey(Departments, related_name="departmental_courses", on_delete=models.CASCADE)
+    faculty = models.ForeignKey(Faculties, related_name="faculty_courses", on_delete=models.CASCADE)
+
+
+    def __str__(self) -> str:
+        return f"{self.code} - {self.title}" 

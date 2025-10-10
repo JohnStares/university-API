e5ad2ae6d94b15754c0a05c0533c3b9140922cb6
JohnStares <wisdom8achor24@gmail.com>
2025-10-10 09:02:24 +0100

    Initial model setup

---
+++ lecturers/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from authentication.models import CustomUser
+
+# Create your models here.
+class Lecturer(models.Model):
+    GENDER = (
+        ('M', 'Male'),
+        ('F', "Female")
+    )
+
+    TITLE = (
+        ('Prof', 'Professor'),
+        ('Dr', 'Doctor')
+    )
+
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
+    date_of_birth = models.DateField(null=False, blank=False)
+    gender = models.CharField(max_length=6, choices=GENDER, null=False, blank=False)
+    title = models.CharField(max_length=6, choices=TITLE)
+
+
+    def __str__(self):
+        return f"{self.title} {self.user.get_full_name()}"
+

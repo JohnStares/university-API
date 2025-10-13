d13537835e3f238e5ee4a8d1998c3bc1b377dac8
JohnStares <wisdom8achor24@gmail.com>
2025-10-13 14:48:31 +0100

    Linked Custom user model to the admin site

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import CustomUser
+
 # Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ["id", "first_name", "last_name", "email", "user_type"]
+    search_fields = ["first_name", "last_name", "email", "user_type"]

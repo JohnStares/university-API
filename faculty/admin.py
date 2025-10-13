8f67fa7a8e2376add7c4e0df729d520a90cbe8c3
JohnStares <wisdom8achor24@gmail.com>
2025-10-13 14:50:01 +0100

    Linked faculty model to the admin site

--- faculty/admin.py
+++ faculty/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Faculties
+
 # Register your models here.
+@admin.register(Faculties)
+class FacultyAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+    search_fields = ["name"]

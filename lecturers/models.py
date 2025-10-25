47cc540177ef83ec719f0237c907e51098f0f3e4
JohnStares <wisdom8achor24@gmail.com>
2025-10-25 15:31:54 +0100

    Added department field

--- lecturers/models.py
+++ lecturers/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from authentication.models import CustomUser
 
+
+from departments.models import Departments
+
 # Create your models here.
 class Lecturer(models.Model):
     GENDER = (
@@ -17,6 +20,7 @@ class Lecturer(models.Model):
     date_of_birth = models.DateField(null=False, blank=False)
     gender = models.CharField(max_length=6, choices=GENDER, null=False, blank=False)
     title = models.CharField(max_length=6, choices=TITLE)
+    department = models.ForeignKey(Departments, on_delete=models.CASCADE)
 
 
     def __str__(self):
